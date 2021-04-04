4ffdd6039bd54e3dcc1fc36cea5c119b1408f5c1
tuannv <tuanngo0898@gmail.com>
2021-04-04 16:06:21 +0700

    mode run reports measurement message (distance + yaw)

--- firmware/PIFWRobot/Core/Src/application/mode/mode_run/mode_run_turtle.c
+++ firmware/PIFWRobot/Core/Src/application/mode/mode_run/mode_run_turtle.c
@@ -9,11 +9,22 @@
 
 #if ROBOT_MODEL==1
 
+TID(gtid_imu_report);
+
+static void mode_run_report_callback(void *ctx){
+	mavlink_message_t msg;
+	float rpy[3];
+	imu_get_rpy(rpy);
+	mavlink_msg_evt_rpy_pack(0,0,&msg,rpy[0],rpy[1],rpy[2]);
+	mav_send_msg(&msg);
+}
+
 void mode_run_init(){
 	// Hardware initialization
 	stepmotor_init();
 
 	// Periodic task initialization
+	gtid_imu_report = timer_register_callback(mode_run_report_callback, MODE_RUN_REPORT_PERIOD, 0, TIMER_MODE_REPEAT);
 }
 
 void mode_run_deinit(){
@@ -26,12 +37,20 @@ void on_mode_run_mavlink_recv(mavlink_message_t *msg){
 		{
 			mavlink_cmd_velocity_t cmd_velocity;
 			mavlink_msg_cmd_velocity_decode(msg, &cmd_velocity);
-//			float gmotor0_speed = (cmd_velocity.v - cmd_velocity.omega);
-//			float gmotor1_speed = (cmd_velocity.v + cmd_velocity.omega);
 			stepmotor_vw(cmd_velocity.v*LINEAR_SPEED_MAX,  cmd_velocity.omega*ANGULAR_SPEED_MAX);
 		}
 		break;
-	default:
+	case MAVLINK_MSG_ID_DISTANCE:
+		{
+			float rpy[3];
+			mavlink_distance_t distance;
+
+			imu_get_rpy(rpy);
+			mavlink_msg_distance_decode(msg, &distance);
+
+			mavlink_msg_measurement_pack(0, 0, msg, distance.x, distance.y, distance.z, distance.r, rpy[2]);
+			mav_send_msg(msg);
+		}
 		break;
 	}
 }
